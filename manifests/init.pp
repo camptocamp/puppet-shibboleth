b73a5ae53a83804925b1ee4d974bd18e8d255444
Marc Fournier <marc.fournier@camptocamp.com>
2010-03-09 20:26:17 +0100

    shibboleth::idp - new class, installs the Shibboleth Identity Provider (v2)

--- manifests/init.pp
+++ manifests/init.pp
@@ -51,5 +51,84 @@ class shibboleth::sp {
 
 }
 
+/*
+
+== Class shibboleth::idp
+
+
+*/
 class shibboleth::idp {
+
+  if ( ! $shibidp_ver ) {
+    $shibidp_ver = "2.1.5"
+  }
+
+  if ( ! $shibidp_home ) {
+    $shibidp_home = "/opt/shibboleth-idp-${shibidp_ver}"
+  }
+
+  if ( ! $shibidp_hostname ) {
+    $shibidp_hostname = "localhost"
+  }
+
+  if ( ! $shibidp_keypass ) {
+    fail("missing mandatory attribute: \$shibidp_keypass.")
+  }
+
+  if ( ! $shibidp_javahome ) {
+    $shibidp_javahome = "/usr"
+  }
+
+  $mirror = "http://shibboleth.internet2.edu/downloads/shibboleth/idp"
+  $url = "${mirror}/${shibidp_ver}/shibboleth-identityprovider-${shibidp_ver}-bin.zip"
+
+  $shibidp_installdir = "/usr/src/shibboleth-identityprovider-${shibidp_ver}"
+
+  common::archive::zip { "${shibidp_installdir}/.installed":
+    source => $url,
+    target => "/usr/src/",
+  }
+
+  # see http://ant.apache.org/faq.html#passing-cli-args
+  exec { "install shibboleth idp":
+    command => "cd ${shibidp_installdir} && ./install.sh",
+    environment => ["JAVA_HOME=${shibidp_javahome}", "ANT_OPTS=-Didp.home.input=${shibidp_home} -Didp.hostname.input=${shibidp_hostname} -Didp.keystore.pass=${shibidp_keypass}"],
+    creates => ["${shibidp_home}/war/idp.war"],
+    require => Common::Archive::Zip["${shibidp_installdir}/.installed"],
+  }
+
+  file { "/opt/shibboleth-idp":
+    ensure  => link,
+    target  => $shibidp_home,
+    require => Exec["install shibboleth idp"],
+  }
+
+  if ( $shibidp_tomcat ) {
+
+    # copy war file from installation dir to tomcat webapp dir.
+    # see also https://spaces.internet2.edu/display/SHIB2/IdPApacheTomcatPrepare for
+    # an alternate method.
+    file { "/srv/tomcat/${shibidp_tomcat}/webapps/idp.war":
+      source  => "file:///${shibidp_home}/war/idp.war",
+      notify  => Service["tomcat-${shibidp_tomcat}"],
+      require => [
+        File["/srv/tomcat/${shibidp_tomcat}/webapps/"],
+        Exec["install shibboleth idp"]],
+    }
+
+    # Copy library shipped with source to tomcat dir.
+    # Don't forget to point the common.loader variable to this directory in
+    # your catalina.properties file !
+    file { "/srv/tomcat/${shibidp_tomcat}/private/endorsed/":
+      ensure  => directory,
+      source  => "file:///${shibidp_installdir}/endorsed/",
+      recurse => true,
+      require => [
+        Common::Archive::Zip["${shibidp_installdir}/.installed"],
+        File["/srv/tomcat/shibb-idp/private/"]],
+      notify  => Service["tomcat-${shibidp_tomcat}"],
+    }
+
+  }
+
 }
