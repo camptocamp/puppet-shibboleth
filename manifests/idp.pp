ac74723c69a57d4e9365a960a783bd9a125bfbbf
Marc Remy <marc.remy@camptocamp.com>
2012-05-29 11:44:26 +0200

    shibboleth::idp: bad mirror

--- manifests/idp.pp
+++ manifests/idp.pp
@@ -41,7 +41,7 @@ class shibboleth::idp {
     $shibidp_javahome = "/usr"
   }
 
-  $mirror = "http://www.shibboleth.net/downloads/identity-provider"
+  $mirror = "http://shibboleth.net/downloads/identity-provider"
   $url = "${mirror}/${shibidp_ver}/shibboleth-identityprovider-${shibidp_ver}-bin.zip"
 
   $shibidp_installdir = "/usr/src/shibboleth-identityprovider-${shibidp_ver}"
