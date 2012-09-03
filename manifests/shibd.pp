b14600549c65936573a96ccf4c04a8e354302a2b
Marc Remy <marc.remy@camptocamp.com>
2012-09-03 11:38:20 +0200

    Add configuration to run shibd daemon not as root

--- manifests/shibd.pp
+++ manifests/shibd.pp
@@ -11,6 +11,21 @@ Requires:
 */
 class shibboleth::shibd {
 
+  $manage_shibd_user = $shibd_user ? {
+        '' => false,
+   default => true,
+  }
+
+  $shibd_local_config_file = $operatingsystem ? {
+    /RedHat|CentOS/ => '/etc/sysconfig/shibd',
+            default => '',
+  }
+
+  $shibd_user = $shibd_user ? {
+         '' => 'root',
+    default => $shibd_user,
+  }
+
   service { "shibd":
     ensure  => running,
     enable  => true,
@@ -22,6 +37,8 @@ class shibboleth::shibd {
 
     file { "/var/run/shibboleth/":
       ensure  => "directory",
+      owner   => $shibd_user,
+      group   => $shibd_user,
       seltype => "httpd_var_run_t",
       notify  => Service["shibd"],
       require => Package["shibboleth"],
@@ -48,6 +65,25 @@ allow httpd_t initrc_t:unix_stream_socket connectto;
     selmodule { "shibd":
       ensure      => present,
       syncversion => true,
+    }
+
+  }
+
+  file { '/var/log/shibboleth':
+    ensure => 'directory',
+    owner  => $shibd_user,
+    group  => $shibd_user,
+    mode   => '750',
+  }
+
+  if $manage_shibd_user {
+
+    file { $shibd_local_config_file:
+      ensure  => present,
+      content => template( "shibboleth/etc/config/shibd.$operatingsystem.erb" ),
+      owner   => 'root',
+      group   => 'root',
+      mode    => 644,
     }
 
   }
