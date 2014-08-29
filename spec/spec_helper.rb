44e7275eda99f483e8e3b4dbe20e69fa2abc0596
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-08-29 15:28:55 +0200

    Update spec/spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
   c.before :each do
-    if ENV['STRICT_VARIABLES'] == 'yes'
+    if Gem::Version.new(`puppet --version`) >= Gem::Version.new('3.5')
       Puppet.settings[:strict_variables]=true
     end
   end
